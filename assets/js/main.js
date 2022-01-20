6896d96b84809593af87db073463c7d2a250e27e
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-01-20 09:54:21 +0100

    comm

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+function changeBg() {
+    document.body.style.background = "rgb(" + document.getElementById("red").value + ", " + document.getElementById("green").value + ", " + document.getElementById("blue").value + ")";
+}
